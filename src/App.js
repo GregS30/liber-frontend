2cdc912c5f45b96c4d8c669bd2088ea70a2827f7
GregS30 <gjschmitt@live.com>
2018-08-10 12:35:57 -0400

    login working, register pending

--- src/App.js
+++ src/App.js
@@ -1,21 +1,71 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
+import React, { Component, Fragment } from 'react';
+import { withRouter } from 'react-router-dom';
+import { connect } from 'react-redux';
+
+// css
 import './App.css';
 
+// adapters
+import Adapter from './adapters/Adapter';
+
+// components
+import Header from './components/Header.js';
+import Navbar from './components/Navbar.js';
+import Footer from './components/Footer.js';
+
+// actions
+import { storeUser, clearUser, getCurrentUser } from './actions';
+
 class App extends Component {
+
+  // auto-login - if token is present in LocalStorage
+  componentDidMount() {
+    this.props.getCurrentUser()
+  }
+
+  // these have to be in our reducer
+
+  handleLogout = () => {
+    Adapter.deleteToken();
+    this.props.clearUser();
+    this.props.history.push('/');
+  }
+
   render() {
+   console.log("App render", this.props)
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+          <Fragment>
+            <div className="header-nav">
+              <Header />
+              <Navbar
+                handleLogout={this.handleLogout}
+                loggedIn={this.props.loggedIn}
+              />
+            </div>
+          </Fragment>
+        <Footer />
       </div>
     );
   }
 }
 
-export default App;
+// redux props
+const mapStateToProps = state => {
+  return {
+    username: state.username,
+    userId: state.userId,
+    loggedIn: state.loggedIn,
+  }
+}
+
+const mapDispatchToProps = dispatch => {
+  return {
+    storeUser: (username, userId) => dispatch(storeUser(username, userId)),
+    clearUser: () => dispatch(clearUser()),
+    getCurrentUser: () => dispatch(getCurrentUser()),
+  }
+}
+
+// see github "withRouter not working if inside redux connect #5256"
+export default withRouter(connect(mapStateToProps, mapDispatchToProps)(App));
